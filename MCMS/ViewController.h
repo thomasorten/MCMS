1bf38f02089f4035d95e121cc7cf033d6db14b54
Thomas Orten <thomas.orten@gmail.com>
2014-05-20 20:26:02 +0200

    Initial Commit

---
+++ MCMS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Orten, Thomas on 20.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MCMSTests/MCMSTests.m
@@ -0,0 +1,34 @@
+//
+//  MCMSTests.m
+//  MCMSTests
+//
+//  Created by Orten, Thomas on 20.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MCMSTests : XCTestCase
+
+@end
+
+@implementation MCMSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
